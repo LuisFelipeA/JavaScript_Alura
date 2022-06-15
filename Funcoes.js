8f269b1269941396e173e46570216fcc0f9d2ef3
LuisFelipeA <felipeadriani2@gmail.com>
2022-06-15 12:57:35 -0300

    Funções

--- Funcoes.js
+++ Funcoes.js
@@ -4,3 +4,10 @@ function imprimeTexto(texto){
 
 imprimeTexto("Oi");
 imprimeTexto("Tudo bem?");
+
+function soma(){
+    return 2 + 2;
+}
+
+console.log(soma());    //podemos usar uma função sem passsar parametros
+imprimeTexto(soma());   //tambem podemos usar uma função dentro de outra
